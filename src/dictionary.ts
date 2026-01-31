92b9ad7cc4041d3adfd5635b34f0b3bfaa2a1ef9
Anton Katzer <accounts@lichta.us>
2026-01-31 12:22:02 +0100

    more stuff

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -24,6 +24,51 @@ export const dictionary = [
     definition: "Unspecified or unneccessary stuff.",
     example: "Ich mag Pizza margarita am liebsten, auf den anderen Sorten ist mir zu viel Gedöns.",
   },
+  {
+    word: "Hackepeter",
+    type: "noun",
+    definition: "Minced raw pork seasoned with onions and spices.",
+    example: "Aus Hackepeter wird Kacke später.",
+  },
+  {
+    word: "Tinnef",
+    type: "noun",
+    definition: "Useless stuff; junk.",
+    example: "Warum kaufst du so viel Tinnef bei Temu?",
+  },
 ];
 
 export default dictionary;
+
+/*
+
+Pipapo
+vermaledeit
+Tja
+Papperlapapp
+Pappnase
+Schlawiner
+Schabernack
+Pissnelke
+Banause
+Arschgeige
+Ulknudel
+Klimbim
+Firlefanz
+krisselig
+wuschig
+Moppelkotze
+Knalltüte
+Flitzpiepe
+Kohldampf
+Wonneproppen
+Etepetete
+malochen
+schlonzig
+knuffig
+friemeln
+assi
+ausgenudelt
+
+
+*/
